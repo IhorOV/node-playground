3ef9af6b6c63589653bb55a923c3198ede2ca64e
Ihor V <i.o.voitovych@gmail.com>
2020-01-21 22:46:33 +0200

    GraphQL basic schema usage

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
 import express from 'express';
-import _ from 'lodash';
-import { listings } from './listings';
-import bodyParser from "body-parser";
+// import _ from 'lodash';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
+// import { listings } from './listings';
+// import bodyParser from "body-parser";
 
 const app = express();
 const port = 9000;
+const server = new ApolloServer({schema});
+server.applyMiddleware({app, path: '/api'});
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.get('/', (_req, res) => res.send('hello Lviv'));
+// app.get('/', (_req, res) => res.send('hello Lviv'));
 
-app.get('/listings', (_req, res) => {
-    return res.send(listings); 
-});
+// app.get('/listings', (_req, res) => {
+//     return res.send(listings); 
+// });
 
-app.post('/del-listing', (req, res) => {
-    const id: string = req.body.id;
-    const filteredList = _.remove(listings, (obj) => obj.id == id)
-    return _.isEqual(listings.length , filteredList.length) ? 
-        res.send('No such item') :
-        res.send(filteredList); 
-});
+// app.post('/del-listing', (req, res) => {
+//     const id: string = req.body.id;
+//     const filteredList = _.remove(listings, (obj) => obj.id == id)
+//     return _.isEqual(listings.length , filteredList.length) ? 
+//         res.send('No such item') :
+//         res.send(filteredList); 
+// });
 
 app.listen(port);
 
